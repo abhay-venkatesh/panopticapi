c0756bb2b4662a4f211206ac183af6d10b5ce94b
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-22 08:27:57 -0500

    stop

---
+++ combine.sh
@@ -0,0 +1,5 @@
+python combine_semantic_and_instance_predictions.py \
+    --semseg_json_file data/stuff.json \
+    --instseg_json_file data/instance.json \
+    --images_json_file data/image_info_test-dev2017.json \
+    --panoptic_json_file data/panoptic.json \
